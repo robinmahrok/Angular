4171095afe4825ae7c840a08a9efe36a6c3b58b0
Robin <robinsinghmahrok@gmail.com>
2020-05-18 15:38:45 +0530

    until Single Page Applications part2

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import{ MatToolbarModule } from '@angular/material/toolbar';
+import{ FlexLayoutModule } from '@angular/flex-layout';
+import{ MatListModule } from '@angular/material/list';
+import {MatGridListModule }from '@angular/material/grid-list'
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { RouterModule } from '@angular/router';
+
+import { MatCardModule} from'@angular/material/card';
+import { MatButtonModule}from '@angular/material/button';
+import 'hammerjs';
+import { from } from 'rxjs';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component'
+import { MenuComponent } from './menu/menu.component';
+import { AppComponent } from './app.component';
+import {DishService } from './services/dish.service';
+import {PromotionService} from './services/promotion.service';
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    AppRoutingModule,
+    MatListModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    RouterModule
+  ],
+  providers: [ DishService, PromotionService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/dishdetail/dishdetail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {Params,ActivatedRoute} from'@angular/router';
+import { Dish } from '../shared/dish';
+import {Location } from'@angular/common';
+import { DishService } from '../services/dish.service';
+
+
+@Component({
+  selector: 'app-dishdetail',
+  templateUrl: './dishdetail.component.html',
+  styleUrls: ['./dishdetail.component.scss']
+})
+export class DishdetailComponent implements OnInit {
+
+    dish:Dish;
+
+  constructor(private dishService:DishService,
+    private route:ActivatedRoute,
+    private location:Location) { }
+
+  ngOnInit(): void {
+      let id=this.route.snapshot.params['id'];
+      this.dish=this.dishService.getDish(id);
+  }
+
+  goBack():void
+  {
+    this.location.back();
+  }
+
+}
